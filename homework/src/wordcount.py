c28dc6aabc8c27e618389ce405cc5b7657aa8aa9
jamurciat <jamurciat@unal.edu.co>
2025-05-11 16:22:50 -0500

    homework2

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,29 +1,18 @@
 import argparse
-import sys
+import os
 
-
-def parse_args():
-
-    parser = argparse.ArgumentParser(description="Count words in files.")
-
-    parser.add_argument(
-        "input",
-        type=str,
-        help="Path to the input folder containing files to process",
-    )
-    parser.add_argument(
-        "output",
-        type=str,
-        help="Path to the output folder for results",
-    )
-
-    parsed_args = parser.parse_args()
-
-    return parsed_args.input, parsed_args.output
+from ._internals.count_words import count_words  # type: ignore
+from ._internals.parse_args import parse_args  # type: ignore
+from ._internals.preprocess_lines import preprocess_lines  # type: ignore
+from ._internals.read_all_lines import read_all_lines  # type: ignore
+from ._internals.split_into_words import split_into_words  # type: ignore
+from ._internals.write_word_counts import write_word_counts  # type: ignore
 
 
 def main():
     input_folder, output_folder = parse_args()
-
-    print("input folder:", input_folder)
-    print("output folder:", output_folder)
+    lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
